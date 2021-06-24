9b19951e415da70e88b13eae2a070a8b702b7203
Bryan Sanchez <bmsa753@gmail.com>
2021-06-24 11:00:32 -0500

    test de admin completados

--- admin_app/tests.py
+++ admin_app/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
+import json 
 #import unittest
 # Create your tests here.
 from main_app.models import Salida, ReservaPasajero, Tour, Reserva, Incluye, NoIncluye, Itinerario, Categoria
@@ -66,14 +67,15 @@ class CategoriaTestCase(TestCase):
         self.assertEqual(salida.capacidad,23)
     '''
     /administrador/reserva-dar-de-baja/
+    En un ambiente de testeo, las views deben usar los metodos save() de los modelos, no los atommicos
+    '''
     def testDarDeBaja(self):
         self.client.login(username='admin222', password='Exploreit2021!')
         reserva_token = "Mmgv2eKwvKd7V2LBQiv1y3zz"
         response = self.client.post('/administrador/reserva-dar-de-baja/', {'reserva_token': reserva_token})
-        print(response)
         reserva = get_object_or_404(Reserva, token=reserva_token)
         self.assertEqual(reserva.de_baja,True)
-    '''
+    
     '''
     /administrador/copiar-tour/
     '''
@@ -81,22 +83,105 @@ class CategoriaTestCase(TestCase):
         self.client.login(username='admin222', password='Exploreit2021!')
         tour_token = "ti5RCHZmtRoPDgcfnvojAV7H"
         response = self.client.post('/administrador/copiar-tour/', {'tour_token': tour_token})
-        print(response)
         nombreTour ="Volcan Chimborazo"
         tourOriginal = get_object_or_404(Tour, nombre=nombreTour)
         tourCopia = get_object_or_404(Tour, nombre="Volcan Chimborazo COPIA")
         self.assertEqual(tourOriginal.nombre+ ' COPIA',tourCopia.nombre)
 
-'''
-nombre + ' COPIA'
-response = self.client.get('/administrador/categorias/', {'categoria': 'FUL','tipo':'NAC'})
-def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Animal.objects.get(name="lion")
-        cat = Animal.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
-'''
+    '''
+    /administrador/reserva-aprobar/
+    En un ambiente de testeo, las views deben usar los metodos save() de los modelos, no los atommicos
+    '''
+    def testReservaAprobar(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        reserva_token = "Mmgv2eKwvKd7V2LBQiv1y3zz"
+        response = self.client.post('/administrador/reserva-aprobar/', {'reserva_token': reserva_token})
+        reserva = get_object_or_404(Reserva, token=reserva_token)
+        self.assertEqual(reserva.estado,"APR")
+    '''
+    /administrador/editar-tour/<slug:slug>/
+    '''
+    '''
+    def testEditarTourGet(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        tour_token = "ti5RCHZmtRoPDgcfnvojAV7H"
+        response = self.client.get('/administrador/editar-tour/'+tour_token)
+        tour = Tour.objects.get(token=tour_token)
+        print(response)
+        self.assertEqual(response.context['tour'],tour)
+    '''
+    def testEditarTourPost(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        tour_token = "ti5RCHZmtRoPDgcfnvojAV7H"
+        tour = Tour.objects.get(token=tour_token)
+        tour.nombre = "Nombre Cambiado del Tour"
+        response = self.client.post('/administrador/editar-tour/'+tour_token,{'tour_data':tour})
+        self.assertEqual(tour.nombre,"Nombre Cambiado del Tour")
+    '''
+    /administrador/registrar-tour/
+    '''
+    def testRegistrarTour(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        tour_token = "ti5RCHZmtRoPDgcfnvojAV7H"
+        tour = Tour.objects.get(token=tour_token)
+        response = self.client.post('/administrador/registrar-tour/',{'tour_data':tour})
+        tour2 = Tour.objects.get(nombre=tour.nombre)
+        self.assertEqual(tour,tour2)
+    '''
+    /administrador/programar-salida/
+    '''
+    def testProgramarSalida(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        tour_token = "ti5RCHZmtRoPDgcfnvojAV7H"
+        tour = Tour.objects.get(token=tour_token)
+        salida_data = {}
+        salida_data['tour']=tour.id
+        salida_data['fecha']='2021-10-21'
+        salida_data['capacidad']='20'
+        json_object = json.dumps(salida_data, indent = 4) 
+        response = self.client.post('/administrador/programar-salida/',{'salida_data':json_object})
+        self.assertEqual(response.status_code,200)
+    '''
+    /administrador/salidas-programadas/<slug:token>/listado-reservas/
+    '''
+    def testListadoReservas(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        salida_token = "DyeISHV8mO8D7BB4Bnqgfx1j"
+        response = self.client.get('/administrador/salidas-programadas/'+salida_token+'/listado-reservas/')
+        self.assertNotEqual(response.status_code,404)
+    '''
+    /administrador/salidas-programadas/<slug:token>/listado-pasajeros/
+    '''
+    def testListadoPasajeros(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        salida_token = "DyeISHV8mO8D7BB4Bnqgfx1j"
+        response = self.client.get('/administrador/salidas-programadas/'+salida_token+'/listado-pasajeros/')
+        self.assertNotEqual(response.status_code,404)
     
+    '''
+    /administrador/tours-registrados/
+    '''
+    def testToursRegistrados(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        response = self.client.get('/administrador/tours-registrados/')
+        self.assertNotEqual(response.status_code,404)
+
+    '''
+    /administrador/
+    '''
+    def testDashboard(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        response = self.client.get('/administrador/')
+        self.assertNotEqual(response.status_code,404)
+
+    '''
+    /administrador/salidas-programadas/
+    '''
+    def testSalidasProgramadas(self):
+        self.client.login(username='admin222', password='Exploreit2021!')
+        response = self.client.get('/administrador/salidas-programadas/')
+        self.assertNotEqual(response.status_code,404)
+
+
     
    
